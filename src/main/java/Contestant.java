2ea2bb0a4d2d08f7ca3447f3ea1a795a136b0d0e
Elizabeth-Giovanniello <ea.giovanniello@gmail.com>
2022-04-24 23:14:02 -0400

    feat: created Contestant class with constructor

--- src/main/java/Contestant.java
+++ src/main/java/Contestant.java
@@ -1,4 +1,18 @@
 
-public class Contestant {
 
+public class Contestant {
+	public String firstName;
+	public String lastName;
+	public int id;
+	public String email;
+	
+	public Contestant(String firstName, String lastName, int id, String email) {
+		this.firstName = firstName;
+		this.lastName = lastName;
+		this.id = id;
+		this.email = email;
+		
+	
+	}
+	
 }
