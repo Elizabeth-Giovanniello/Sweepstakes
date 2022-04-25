875693cedf38e0289bb50784bea9c50556029e6b
Elizabeth-Giovanniello <ea.giovanniello@gmail.com>
2022-04-24 23:21:36 -0400

    created Sweepstakes class with constructor and two methods, RegisterContestants and PickWinner

--- src/main/java/Contestant.java
+++ src/main/java/Contestant.java
@@ -5,13 +5,16 @@ public class Contestant {
 	public String lastName;
 	public int id;
 	public String email;
+	public boolean winner;
 	
-	public Contestant(String firstName, String lastName, int id, String email) {
+	public Contestant(String firstName, String lastName, int id, String email, boolean winner) {
 		this.firstName = firstName;
 		this.lastName = lastName;
 		this.id = id;
 		this.email = email;
-		
+		this.winner = winner;
+	
+		//TODO getters and setters for this class. tbd because I'm not sure for what
 	
 	}
 	
---
+++ src/main/java/Sweepstakes.java
@@ -0,0 +1,16 @@
+
+public class Sweepstakes {
+	public String name;
+
+	public Sweepstakes(String name) {
+		this.name = name;
+	}
+	
+	public void RegisterContestants() {
+		//tbd
+	}
+	
+	public void PickWinner() {
+		//also tbd, possibly need change void to int or String, as it will return the winner(?)
+	}
+}
